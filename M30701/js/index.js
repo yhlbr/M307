fa112fa3a477a300e779c8ddc533eabc9cbe3bfb
KR1PC04 <kr1pc04@lan.spirig-schulungscenter.ch>
2019-02-04 12:02:09 +0100

    Farben loading

--- M30701/js/index.js
+++ M30701/js/index.js
@@ -1,9 +1,30 @@
 $(function () {
-    $.info("Seite geladen.");
+    $.info("Seite geladen");
+    $('#load').click(function () {
+        $.ajax({
+            url: 'http://www.colr.org/json/colors/random/3',
+            dataType: 'json',
+            success: function (data) {
+                $.info("Farben erhalten");
+                $.info(data);
+                for (var idx in data.colors) {
+                    var color = data.colors[idx].hex;
+                    var el = document.createElement('p');
+                    $(el).text(color).css({
+                        color: '#' + color
+                    });
+                    $('#colors').append(el);
+                }
+            }
+        });
+    });
+    $('#clear').click(function () {
+        $('#colors').empty();
+    });
 });
 
 $.extend({
     info: function (value) {
-        console.log("Info: " + value);
+        console.log(value);
     }
 });
---
+++ M307Noten/js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+    
+});
+
+$.extend({
+    info: function (value) {
+        console.log(value);
+    }
+});
